8d179a064463d7184822c9ea9b0f97da582ef549
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-04-15 02:14:54 -0700

    getting allevents of single city using getStaticPaths & getStaticProps

--- src/pages/events/[cat]/index.js
+++ src/pages/events/[cat]/index.js
@@ -1,18 +1,70 @@
 
 import React from 'react'
+import Image from 'next/image'
+
+export async function getStaticPaths(){
+    const { events_categories } = await import('../../../data/data.json')
+    const allPaths=events_categories.map((event)=>{
+        return {
+            params:{
+                cat:event.id.toString()
+            }
+        }
+        })
+
+        console.log(allPaths)
+    
+    return{
+        paths:allPaths,
+        fallback:false
+    }
+}
 
 
+export async function getStaticProps(context){
+    const { allEvents } = await import('../../../data/data.json')
+    console.log('Context>>>',context)
+    const id=context?.params.cat
+    console.log(id)
 
+    const data=allEvents.filter((data)=>{
+        const city=data.city.toLowerCase()
+        return city==id
+    })
+    console.log('CITY DATA>>',data)
+    return{
+        props:{
+          data:data
+        }
+    }
+}
 
-const EventCat = () => {
+const EventCat = ({data}) => {
   return (
-    <div><h1>Events in London</h1>
-    <a href='/events/event1'>Event 1</a>
-    <a href=''>Event 2</a>
-    <a href=''>Event 3</a>
-    <a href=''>Event 4</a>
+    <>
+       {
+    data.map((data)=>{
+        return(
+            <div>
 
-    </div>
+                <h2>Event in </h2>
+                          <Image width={300} height={300} alt={data.title} src={data.image} />
+                          <div>
+                            <h4>{data.title}</h4>
+                            <span>City: {data.city.toUpperCase()}</span>
+                            <p>
+                                {data.description}
+                            </p>
+                          </div>
+
+            </div>
+        )
+      
+        
+    })
+   }
+    </>
+
   )
 }
 
--- src/pages/events/event1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Event1 = () => {
-  return (
-    <div>event1 page</div>
-  )
-}
-
-export default Event1
