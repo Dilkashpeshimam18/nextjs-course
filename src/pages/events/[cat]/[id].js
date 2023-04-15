40bf842a6282c0c7cba286cee998a10f38c46e3d
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-04-15 03:07:24 -0700

    showing single events detail

--- src/pages/events/[cat]/[id].js
+++ src/pages/events/[cat]/[id].js
@@ -1,8 +1,58 @@
 import React from 'react'
+import Image from 'next/image'
 
-const SingleEvents = () => {
+export async function getStaticPaths() {
+  const { allEvents } = await import('../../../data/data.json')
+  const allPaths = allEvents.map((eve) => {
+    return {
+      params: {
+        cat: eve.city.toLowerCase(),
+        id: eve.id.toString()
+      }
+    }
+  })
+
+
+  return {
+    paths: allPaths,
+    fallback: true
+
+  }
+
+}
+
+export async function getStaticProps(context) {
+  const { allEvents } = await import('../../../data/data.json')
+  console.log('Single Event Context>>>', context)
+  const id = context?.params.id
+
+  const data = allEvents.filter((data) => {
+    return data.id == id
+  })
+
+
+  return {
+    props: {
+      data: data
+    }
+  }
+
+}
+
+const SingleEvents = ({ data }) => {
   return (
-    <div><h1>Single Events</h1></div>
+    <div>
+      <Image width={300} height={300} alt={data[0].title} src={data[0].image} />
+
+      <h1>{data[0].title}</h1>
+      <div>
+        <span>City: {data[0].city.toUpperCase()}</span>
+        <p>
+          {data[0].description}
+        </p>
+      </div>
+
+    </div>
   )
 }
 
--- src/pages/events/[cat]/index.js
+++ src/pages/events/[cat]/index.js
@@ -2,70 +2,67 @@
 import React from 'react'
 import Image from 'next/image'
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
     const { events_categories } = await import('../../../data/data.json')
-    const allPaths=events_categories.map((event)=>{
+    const allPaths = events_categories.map((event) => {
         return {
-            params:{
-                cat:event.id.toString()
+            params: {
+                cat: event.id.toString()
             }
         }
-        })
+    })
 
-        console.log(allPaths)
-    
-    return{
-        paths:allPaths,
-        fallback:false
+    return {
+        paths: allPaths,
+        fallback: false
     }
 }
 
 
-export async function getStaticProps(context){
+export async function getStaticProps(context) {
     const { allEvents } = await import('../../../data/data.json')
-    console.log('Context>>>',context)
-    const id=context?.params.cat
-    console.log(id)
+    const id = context?.params.cat
 
-    const data=allEvents.filter((data)=>{
-        const city=data.city.toLowerCase()
-        return city==id
+    const data = allEvents.filter((data) => {
+        const city = data.city.toLowerCase()
+        return city == id
     })
-    console.log('CITY DATA>>',data)
-    return{
-        props:{
-          data:data
+    return {
+        props: {
+            data: data
         }
     }
 }
 
-const EventCat = ({data}) => {
-  return (
-    <>
-       {
-    data.map((data)=>{
-        return(
-            <div>
+const EventCat = ({ data }) => {
+    return (
+        <>
+            {
+                data.map((data) => {
+                    return (
+                        <div>
+                         <a href={`/events/${data.city}/${data.id}`} >
+                         <Image width={300} height={300} alt={data.title} src={data.image} />
+                         <h4>{data.title}</h4>
 
-                <h2>Event in </h2>
-                          <Image width={300} height={300} alt={data.title} src={data.image} />
-                          <div>
-                            <h4>{data.title}</h4>
-                            <span>City: {data.city.toUpperCase()}</span>
-                            <p>
-                                {data.description}
-                            </p>
-                          </div>
+                          </a>
+                         <div>
+                                <span>City: {data.city.toUpperCase()}</span>
+                                <p>
+                                    {data.description}
+                                </p>
+                            </div>
+                           
 
-            </div>
-        )
-      
-        
-    })
-   }
-    </>
+                        </div>
+                    )
 
-  )
+
+                })
+            }
+        </>
+
+    )
 }
 
 export default EventCat
